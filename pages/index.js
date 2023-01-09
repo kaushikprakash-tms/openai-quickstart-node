f24ed7ee92cfde33301f9b170f40377c83dde713
Kaushik Prakash <kprakash@totalmortgage.com>
2023-01-09 18:21:40 -0500

    v2 changes

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,25 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [question, setQuestion] = useState("");
+  const [lastQuestion, setLastQuestion] = useState("");
   const [result, setResult] = useState();
   const [isLoading, setIsLoading] = useState(false);
     
-    async function handleClick(event) {
-      setIsLoading(true);
-      await askGPT(event);
-      setIsLoading(false);
-    }
+  async function handleClick(event) {
+    setIsLoading(true);
+    await askGPT(event);
+    setIsLoading(false);
+  }
 
+  function handleFAQ(event) {
+    setQuestion(event.target.innerText);
+    // setIsLoading(false);
+  }
   async function askGPT(event) {
     event.preventDefault();
+    setLastQuestion("");
+    setResult("");
+    
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -29,8 +37,10 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
+      setResult(data.result.replace(/\n/g, "<br />").replace("<br /><br />","<br />"));
+      setLastQuestion(question);
       setQuestion("");
+
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -58,7 +68,18 @@ export default function Home() {
           />
           <button onClick={handleClick} disabled={isLoading}>{isLoading ? 'Thinking...' : 'Submit'}</button>
           </form>
-        <div className={styles.result}>{result}</div>
+
+        <div className={styles.question}>{lastQuestion}</div>
+        <div className={styles.result}>{ <div dangerouslySetInnerHTML={{__html: result}} />}</div>
+
+        <h4>People also ask</h4>
+          <div className={styles.faq}>
+            <q onClick={handleFAQ}>What is the down payment criteria on an FHA loan ?</q>
+            <q onClick={handleFAQ}>What is the minimum down payment for a house in Wisconsin?</q>
+            <q onClick={handleFAQ}>How soon can you get a mortgage after Chapter 13 discharge?</q>
+            <q onClick={handleFAQ}>How long after Chapter 13 Can I get FHA?</q>
+          </div>
+
       </main>
     </div>
   );
